c83eb87f803baf77462ddf6f18ca13d7cc4dacca
Youngwoo Choi <yw823x@daum.net>
2023-04-28 11:08:04 +0900

    [update] InterruptedException

--- src/main/java/com/ll/simpleDb/SQLRuntimeException.java
+++ src/main/java/com/ll/simpleDb/SQLRuntimeException.java
@@ -3,6 +3,17 @@ package com.ll.simpleDb;
 import java.sql.SQLException;
 
 public class SQLRuntimeException extends RuntimeException {
+    public SQLRuntimeException(String s) {
+        System.out.println(s);
+    }
+
     public SQLRuntimeException(String s, SQLException e) {
+        System.out.println(s);
+        e.printStackTrace();
+    }
+
+    public SQLRuntimeException(String s, InterruptedException e) {
+        System.out.println(s);
+        e.printStackTrace();
     }
 }
